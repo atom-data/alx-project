a1112810d3dcc1facb127a27a4a7f6ca31b94f89
atom-data <alatatomondi@gmail.com>
2024-01-02 17:05:30 +0000

    updated merge_data_for_variable_year

--- global.R
+++ global.R
@@ -1,63 +1,100 @@
-###--- Loading and pre-processing of data---###
-###--Set the working directory
-setwd("alx-project")
+###--- 1. Load libraries and set working directory ---###
 
-###---Load the required packages
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(readr, dplyr, sf, leaflet)  # Load libraries
+pacman::p_load(readr, dplyr, sf, leaflet)  # Load libraries efficiently
 
-###---Load the required dataset
-dhs <- read_csv("dhs-quickstats_subnational_ken.csv")
-provinces <- read_csv("province.csv")
-kenya_shp <- st_read(dsn = getwd(), layer = "County")
+setwd("alx-project")  # Set working directory
 
-###--- Clean up the county names of kenya_shp
-replace_fun <- function(original, new){
-  return(kenya_shp %>% mutate(COUNTY = replace(COUNTY,
-                                               COUNTY == original,
-                                               new )))
-}
+###--- 2. Load and preprocess data ---###
 
-kenya_shp <- replace_fun("Tharaka", "Tharaka-Nithi")
-kenya_shp <- replace_fun("Keiyo-Marakwet", "Elgeyo Marakwet")
-kenya_shp <- replace_fun("Trans Nzoia", "Trans-Nzoia")
-
-###--- IndicatorId of interest
-indicatorId <- c("RH_DELP_C_DHF", "FE_FRTR_W_TFR", "CH_DIAT_C_ORT",
-                 "ED_EDUC_W_SEH", "CM_ECMR_C_U5M")
-
-dhs_select <- dhs %>% right_join(provinces,
-                         by = c("Location" = "Region")) %>%
-  filter(IndicatorId %in% indicatorId) %>%
-  group_by(Province, IndicatorId, SurveyYear) %>%
-  mutate(Val = mean(as.numeric(Value), na.rm = T)) %>%
-  ungroup()
-
-###---Join the two data files to be one
-kenya_province <- kenya_shp %>%
-  left_join(provinces, by = c("COUNTY" = "Region"))
-
-###---create a basemap for the data
-basemap <- leaflet(options = leafletOptions(minZoom = 5)) %>%
-  setMaxBounds(33.911819, -4.702271, 41.906258, 5.430648) %>%
-  setView(lng=37.818, lat=0.606, zoom=7) %>%
-  addTiles() %>%
-  addPolygons(
-    data = kenya_shp,
-    label = ~COUNTY,
-    weight = 3,
-    color = "black",
-    fillColor = "white",
-    fillOpacity = 0.0
+# Load and preprocess shapefile with error handling
+load_shapefile <- function() {
+  tryCatch(
+    {
+      kenya_counties_sf <- st_read(dsn = getwd(), layer = "County") %>%
+        mutate(
+          county_name = case_when(
+            COUNTY == "Tharaka" ~ "Tharaka-Nithi",
+            COUNTY == "Keiyo-Marakwet" ~ "Elgeyo Marakwet",
+            COUNTY == "Trans Nzoia" ~ "Trans-Nzoia",
+            TRUE ~ COUNTY
+          )
+        )
+    },
+    error = function(e) {
+      stop("Error loading shapefile: ", e$message)
+    }
   )
-
-###--- Define a function to merge the required variable with the kenyan shapefile
-kenya_dhs <- function(data = kenya_province, variable, year) {
-  dhs_select_filter <- dhs_select %>%
-    filter(Indicator == variable) %>%
-    filter(SurveyYear == year)
-  data <- data %>%
-    left_join(dhs_select_filter,
-                             by = "Province")
-  return(data)
 }
+
+# Load and filter survey data with error handling
+load_survey_data <- function() {
+  tryCatch(
+    {
+      dhs_survey_data <- read_csv("dhs-quickstats_subnational_ken.csv") %>%
+        filter(IndicatorId %in% c("RH_DELP_C_DHF", "FE_FRTR_W_TFR", "CH_DIAT_C_ORT", "ED_EDUC_W_SEH", "CM_ECMR_C_U5M")) %>%
+        right_join(read_csv("province.csv"), by = c("Location" = "Region")) %>%  # Read province data as CSV
+        group_by(province_name = Province, indicator_id = IndicatorId, survey_year = SurveyYear) %>%
+        mutate(mean_value = mean(as.numeric(Value), na.rm = TRUE)) %>%
+        ungroup()
+    },
+    error = function(e) {
+      stop("Error loading survey data: ", e$message)
+    }
+  )
+}
+
+###--- 3. Join data ---###
+
+merge_data <- function() {
+  tryCatch(
+    {
+      kenya_counties_sf <- load_shapefile()
+      dhs_survey_data <- load_survey_data()
+      
+      kenya_merged_data <- kenya_counties_sf %>%
+        left_join(dhs_survey_data, by = c("county_name" = "Location"))
+      
+      return(kenya_merged_data)
+    },
+    error = function(e) {
+      message("An error occurred during data merging: ", e$message)
+    }
+  )
+}
+
+##--- 4. Visualization functions (unchanged) ---###
+
+# Create basemap function
+create_basemap <- function(kenya_counties_sf) {
+  tryCatch(
+    {
+      leaflet(options = leafletOptions(minZoom = 5)) %>%
+        setMaxBounds(33.911819, -4.702271, 41.906258, 5.430648) %>%
+        setView(lng = 37.818, lat = 0.606, zoom = 7) %>%
+        addTiles() %>%
+        addPolygons(
+          data = kenya_counties_sf,
+          label = ~county_name,
+          weight = 3,
+          color = "black",
+          fillColor = "white",
+          fillOpacity = 0.0
+        )
+    },
+    error = function(e) {
+      stop("Error creating basemap: ", e$message)
+    }
+  )
+}
+
+
+# Function to filter data for a given variable and year
+merge_data_for_variable_year <- function(data, variable, year) {
+  data %>%
+    filter(indicator_id == variable, survey_year == year)
+}
+
+
+
+
