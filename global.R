eba466e68d758a2a8248cdc5c37e532244203df7
atom-data <alatatomondi@gmail.com>
2024-01-03 06:33:06 +0000

    improvement on leaflet

--- global.R
+++ global.R
@@ -1,14 +1,10 @@
-###--- 1. Load libraries and set working directory ---###
-
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(readr, dplyr, sf, leaflet)  # Load libraries efficiently
 
 setwd("alx-project")  # Set working directory
 
-###--- 2. Load and preprocess data ---###
-
-# Load and preprocess shapefile with error handling
-load_shapefile <- function() {
+# Function to load and preprocess shapefile
+load_and_preprocess_shapefile <- function() {
   tryCatch(
     {
       kenya_counties_sf <- st_read(dsn = getwd(), layer = "County") %>%
@@ -27,11 +23,11 @@ load_shapefile <- function() {
   )
 }
 
-# Load and filter survey data with error handling
-load_survey_data <- function() {
+# Function to load and filter survey data
+load_and_filter_survey_data <- function() {
   tryCatch(
     {
-      dhs_survey_data <- read_csv("dhs-quickstats_subnational_ken.csv") %>%
+      filtered_survey_data <- read_csv("dhs-quickstats_subnational_ken.csv") %>%
         filter(IndicatorId %in% c("RH_DELP_C_DHF", "FE_FRTR_W_TFR", "CH_DIAT_C_ORT", "ED_EDUC_W_SEH", "CM_ECMR_C_U5M")) %>%
         right_join(read_csv("province.csv"), by = c("Location" = "Region")) %>%  # Read province data as CSV
         group_by(province_name = Province, indicator_id = IndicatorId, survey_year = SurveyYear) %>%
@@ -44,16 +40,17 @@ load_survey_data <- function() {
   )
 }
 
-###--- 3. Join data ---###
-
+# Function to merge data
 merge_data <- function() {
   tryCatch(
     {
-      kenya_counties_sf <- load_shapefile()
-      dhs_survey_data <- load_survey_data()
+      kenya_counties_sf <- load_and_preprocess_shapefile()
+      filtered_survey_data <- load_and_filter_survey_data()
+      
+      # Validate data before merging (consider adding checks here)
       
       kenya_merged_data <- kenya_counties_sf %>%
-        left_join(dhs_survey_data, by = c("county_name" = "Location"))
+        left_join(filtered_survey_data, by = c("county_name" = "Location"))
       
       return(kenya_merged_data)
     },
@@ -63,38 +60,54 @@ merge_data <- function() {
   )
 }
 
-##--- 4. Visualization functions (unchanged) ---###
-
-# Create basemap function
-create_basemap <- function(kenya_counties_sf) {
-  tryCatch(
-    {
-      leaflet(options = leafletOptions(minZoom = 5)) %>%
-        setMaxBounds(33.911819, -4.702271, 41.906258, 5.430648) %>%
-        setView(lng = 37.818, lat = 0.606, zoom = 7) %>%
-        addTiles() %>%
-        addPolygons(
-          data = kenya_counties_sf,
-          label = ~county_name,
+create_basemap <- function(data) {
+  tryCatch({
+    leaflet(options = leafletOptions(
+      minZoom = 5,
+      zoomControl = FALSE  # Remove default zoom buttons for custom control
+    )) %>%
+      setMaxBounds(33.911819, -4.702271, 41.906258, 5.430648) %>%
+      setView(lng = 37.818, lat = 0.606, zoom = 7) %>%
+      addProviderTiles(providers$OpenStreetMap, group = "OpenStreetMap") %>%
+      addProviderTiles(providers$Esri.WorldImagery, group = "Esri") %>%
+      addProviderTiles(providers$Stamen.Terrain, group = "Terrain") %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+      addPolygons(
+        data = data,
+        label = ~county_name,
+        weight = 2,
+        color = "black",
+        fillColor = "lightblue",
+        fillOpacity = 0.25,
+        group = ~province_name,  # Group counties by province_name
+        highlight = highlightOptions(
           weight = 3,
           color = "black",
-          fillColor = "white",
-          fillOpacity = 0.0
+          bringToFront = TRUE,
+          group = ~province_name  # Highlight entire province group
         )
-    },
-    error = function(e) {
-      stop("Error creating basemap: ", e$message)
-    }
-  )
+      ) %>%
+      addLayersControl(
+        baseGroups = c("OpenStreetMap", "Esri", "Carto", "Terrain", "Toner"),
+        options = layersControlOptions(collapsed = TRUE),
+        position = "bottomright"
+      ) %>%
+      addEasyButton(easyButton(
+        icon = "fa-globe", title = "Zoom to Level 7",
+        onClick = JS("function(btn, map){ map.setZoom(7); }"))) %>%  # Custom zoom control
+      addEasyButton(easyButton(
+        icon = "fa-arrows-alt", title = "Reset View",
+        onClick = JS("function(btn, map){ map.fitBounds(map.getBounds()); }")))  # Reset view button
+  },
+  error = function(e) {
+    stop("Error creating basemap: ", e$message)
+  })
 }
 
-
 # Function to filter data for a given variable and year
-merge_data_for_variable_year <- function(data, variable, year) {
+filter_data_for_variable_year <- function(data, variable, year) {
   data %>%
     filter(indicator_id == variable, survey_year == year)
 }
 
-
-
-
+          
