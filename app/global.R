7f51565ff4bcdd0f3e248cfee64d26e75ed2befd
Tom Alata <alatatomondi@gmail.com>
2024-01-07 19:52:14 +0300

    updated global file and file structure

---
+++ app/global.R
@@ -0,0 +1,11 @@
+###---Load the required packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(readr, dplyr, sf,
+               leaflet, ggplot2, tidyr,
+               htmltools, plotly, DT)  # Load libraries
+
+# Data loading and preprocessing
+source("code/data_loading_and_preprocessing.R")  # Load functions from a separate file
+
+# Visualization functions
+source("code/visualization_functions.R")  # Load functions from a separate file
--- code/visualization_functions.R
+++ code/visualization_functions.R
@@ -23,3 +23,50 @@ create_basemap <- function(data) {
       onClick = JS("function(btn, map){ map.fitBounds(map.getBounds()); }"))) %>%
     hideGroup("province_boundaries")  # Hide initial boundaries
 }
+
+# Function for legend creation 
+create_legend <- function(data, values, colors, title = "Legend") {
+  data %>% addLegend(
+    position = "topright",  # position
+    pal = colors,
+    values = values,
+    title = title,
+    labFormat = labelFormat(digits = 2),  # customized label formatting
+    opacity = 1
+  )
+}
+
+# create a choropleth map function:
+create_choropleth_map <- function(data, variable, year) {
+  # Validate data (example)
+  if (is.null(data) || is.null(variable) || is.null(year)) {
+    stop("Invalid input data or parameters.")
+  }
+  
+  filtered_data <- data %>% filter(indicator_id == variable, survey_year == year)
+  pal <- colorNumeric("YlOrRd", domain = filtered_data$mean_value, na.color = "transparent")
+  
+  leafletProxy("map") %>%
+    clearShapes() %>%
+    addPolygons(
+      data = filtered_data,
+      group = "province_boundaries",
+      layerId = ~province_name,
+      stroke = FALSE,
+      smoothFactor = 0.2,
+      fillOpacity = 0.8,
+      fillColor = ~pal(mean_value),
+      highlight = highlightOptions(
+        weight = 5,
+        color = "#666",
+        bringToFront = TRUE
+      ),
+      popup = ~paste0("<strong>Province:</strong> ", province_name, "<br/>",
+                      "<strong>Counties:</strong> ", paste(county_name, collapse = ", "), "<br/>",
+                      "<strong>Variable:</strong> ", variable, "<br/>",
+                      "<strong>Year:</strong> ", year, "<br/>",
+                      "<strong>Mean Value:</strong> ", mean_value)
+    ) %>%
+    showGroup("province_boundaries") %>%  # Show the hidden group
+    create_legend(values = ~filtered_data$mean_value, colors = pal)
+}
