9b9aa20009d3c5382b57360ced92a6909c915c6c
Tom Alata <alatatomondi@gmail.com>
2024-01-14 12:02:40 +0300

    defined plot functions

--- code/plot_functions.R
+++ code/plot_functions.R
@@ -1,15 +1,29 @@
 # Load required packages
-pacman::p_load(ggplot2, plotly, dplyr)
+pacman::p_load(plotly, dplyr)
 
 create_plotly_plot <- function(data, region, province, variable, year, plot_type, scatter_var) {
   
   # Filter data based on user selections
-  filtered_data <- data %>%
-    filter(
-      if (region == "Province") province_name == province else county_name == province,
-      indicator_label == variable,
-      survey_year == year
-    )
+  # Filter data based on user selections and plot type
+  filtered_data <- if (plot_type == "Time Series") {
+    data %>%
+      filter(
+        if (region == "Province") province_name == province else county_name == province,
+        indicator_label == variable
+      )
+  } else if (plot_type == "Histogram") {
+    data %>%
+      filter(indicator_label == variable, survey_year == year) %>%
+      distinct(province_name, mean_value)  # Keep only unique values per province
+  } else if (plot_type == "Scatter") {
+    data %>%
+      select(indicator_label, survey_year, mean_value) %>%
+      filter(indicator_label %in% c(variable, scatter_var), survey_year == year) %>%  # Filter for both variables
+      pivot_wider(names_from = "indicator_label", values_from = "mean_value")  # Reshape
+  } else {
+    # Handle invalid plot_type
+    data.frame()  # Return empty data frame
+  }
   
   # Create the plot based on plot_type
   if (plot_type == "Time Series") {
@@ -36,10 +50,10 @@ create_plotly_plot <- function(data, region, province, variable, year, plot_type
         paper_bgcolor = "whitesmoke"                  # Background color
       )
   } else if (plot_type == "Scatter") {
-    plot_ly(filtered_data, x = ~mean_value, y = ~get(scatter_var)) %>%
+    plot_ly(filtered_data, x = ~get(variable), y = ~get(scatter_var)) %>%  # Access variables directly
       add_markers() %>%
       layout(
-        title = paste("Scatter Plot of", variable, "against", scatter_var, "in", province, "for", year),
+        title = paste("Scatter Plot of", variable, "against", scatter_var, "for", year),
         xaxis = list(title = variable),
         yaxis = list(title = scatter_var),
         font = list(family = "sans-serif", size = 14), # Consistent font
